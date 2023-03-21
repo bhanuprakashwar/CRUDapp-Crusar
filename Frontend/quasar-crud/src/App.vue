8a88f91b4548c59d0884d2fa3ce5d4d775b857c7
Bhanu <likebhanuprakash@gmail.com>
2023-03-21 11:40:34 +0530

    Included sample frontend quasar

---
+++ Frontend/quasar-crud/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Sample Quasar</div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'LayoutDefault',
+  components: {
+  },
+  setup () {
+    return {
+      leftDrawerOpen: ref(false)
+    }
+  }
+}
+</script>
