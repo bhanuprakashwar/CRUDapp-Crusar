b41ff7de2c09fea685c04e3227fc7b52fd129bdb
Bhanu <likebhanuprakash@gmail.com>
2023-03-21 11:51:26 +0530

    Included crudapp component and service

--- Frontend/quasar-crud/src/App.vue
+++ Frontend/quasar-crud/src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>Sample Quasar</div>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <CRUDApp />
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
 import { ref } from 'vue'
+import CRUDApp from '../src/components/CRUDApp.vue'
 
 export default {
   name: 'LayoutDefault',
+
   components: {
+    CRUDApp
   },
+
   setup () {
     return {
       leftDrawerOpen: ref(false)
